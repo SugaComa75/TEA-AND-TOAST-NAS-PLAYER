1044bdead09096ba7b34e8d8cc851014b159c2e3
SugaComa <sugacoma@rik-sprint.co.uk>
2026-08-30 11:28:55 +0100

    mobile layout cleanup

--- config/release.php
+++ config/release.php
@@ -2,8 +2,8 @@
 declare(strict_types=1);
 
 return [
-    'version' => '0.3.2',
-    'ref' => '0.3.2',
+    'version' => '0.3.2a',
+    'ref' => '0.3.2a',
     'repository' => 'SugaComa75/Tea-and-Toast-NAS-Player',
     'manifest' => 'release-manifest.json',
 ];
